ca72b6ec7e50130e94f95f3c81b2a0fd0b522411
sixfortyfour <sixfortyfour@users.noreply.github.com>
2025-03-18 19:42:44 +0000

    Removed test function

--- api/src/functions/TestFunction.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.http('test', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        return { body: `Hello, from the API!` };
-    }
-});
