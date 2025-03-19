f6095ac80c2f2e26b96889c165f3045cd48b8788
sixfortyfour <sixfortyfour@users.noreply.github.com>
2025-03-19 15:10:47 +0000

    Refactor

--- api/src/functions/EncryptFunction.js
+++ api/src/functions/EncryptFunction.js
@@ -6,7 +6,7 @@ const redis = new Redis({
     token: process.env.UPSTASH_REDIS_REST_TOKEN,
   })
 
-app.http('encryptfunction', {
+app.http('encrypt', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
