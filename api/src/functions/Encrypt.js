b8d1b3e0b61831495a75852fd657be91bd935c55
sixfortyfour <sixfortyfour@users.noreply.github.com>
2025-03-19 16:18:35 +0000

    Added Redis

--- api/src/functions/Encrypt.js
+++ api/src/functions/Encrypt.js
@@ -1,22 +1,20 @@
 const { app } = require('@azure/functions');
 const { Redis } = require('@upstash/redis');
 
-/*
 const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL,
     token: process.env.UPSTASH_REDIS_REST_TOKEN,
   })
-*/
 
 app.http('encrypt', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        //const count = await redis.incr("counter");
+        const count = await redis.incr("counter");
 
-        //console.log(`Counter: ${count}`);
+        console.log(`Counter: ${count}`);
 
-        //return { body: `${count}` };
+        return { body: `${count}` };
 
         return { body: 'Hello from Encrypt' };
     }
